01e05a0d4a017e9b02b9dd6db038591c607ae4a8
leosakharov <leo.sakharoff@gmail.com>
2026-02-10 15:33:05 +0100

    Refactor all routes and more
    Refactor all routes and update the html files + the control script

--- control.sh
+++ control.sh
@@ -2,18 +2,15 @@
 
 if [ "$1" = "init" ]; then
 
-    if [ -f "/tmp/minitwit.db" ]; then 
+    if [ -f "/tmp/minitwit.db" ]; then
         echo "Database already exists."
         exit 1
     fi
     echo "Putting a database to /tmp/minitwit.db..."
-    python -c"from minitwit import init_db;init_db()"
-elif [ "$1" = "startprod" ]; then
-     echo "Starting minitwit with production webserver..."
-     nohup "$HOME"/.local/bin/gunicorn --workers 4 --timeout 120 --bind 0.0.0.0:5000 minitwit:app > /tmp/out.log 2>&1 &
+    sqlite3 /tmp/minitwit.db < schema.sql
 elif [ "$1" = "start" ]; then
     echo "Starting minitwit..."
-    nohup "which python" minitwit.py > /tmp/out.log 2>&1 &
+    nohup go run main.go > /tmp/out.log 2>&1 &
 elif [ "$1" = "stop" ]; then
     echo "Stopping minitwit..."
     pkill -f minitwit
